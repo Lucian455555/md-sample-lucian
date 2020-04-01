249544f4387a44ab14c5a06257b5c248c88008b5
Lucian455555 <60394006+Lucian455555@users.noreply.github.com>
2020-04-01 15:14:39 +0300

    Add files via upload

---
+++ md-sample-L-master/src/js/components/footer/footer.js
@@ -0,0 +1,8 @@
+import footerTemplate from '../../templates/footer.hbs';
+var footerContainer = document.getElementById('footer-hbs');
+/* logic with nav links */
+var navigationLinks = []
+if (footerContainer) {
+ footerContainer.innerHTML = footerTemplate({ navigationLinks });
+}
+ 
